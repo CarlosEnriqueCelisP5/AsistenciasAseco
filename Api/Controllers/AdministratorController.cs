18ca2116157cd8c9a4e9491eadaf851485c86c7d
Carlos Enrique Celis P <CarlosEnriqueCelisP5@gmail.com>
2023-01-10 20:32:22 -0500

    Prueba CC

---
+++ Api/Controllers/AdministratorController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class AdministratorController : ControllerBase
+    {
+        [HttpGet("Prueba")]
+        public IActionResult Prueba()
+        {
+            return Ok("Hola Mundo");
+        }
+    }
+}
